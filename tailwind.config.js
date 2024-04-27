c30b2ebeb6cddf6cbe87b2c61d677ff4b3f1726f
Arnab Roy <rarnab225@gmail.com>
2024-04-27 11:18:07 +0530

    real estate project 27/04/24

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize:{
+        sm:"0.75rem",
+        base:"0.875rem",
+        md:"1rem",
+        lg:"1.25rem",
+        xl:"1.5rem",
+        "2xl":"1.75rem",
+        "3xl":"2.25rem"
+      },
+      backgroundImage: {
+        'team-bg-image': "url('/public/teams/team.png')",
+        
+      }
+    },
+  },
+  plugins: [],
+}
